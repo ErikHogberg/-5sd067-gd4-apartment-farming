716c84412b98de3c01affaadc29dce6d57d9769b
Erik Högberg <erik.hogberg.9945@student.uu.se>
2020-02-17 12:01:48 +0100

    camera movement spots/buttons working

--- Assets/Scripts/Logic/ClickToDeleteScript.cs
+++ Assets/Scripts/Logic/ClickToDeleteScript.cs
@@ -6,6 +6,7 @@ public class ClickToDeleteScript : MonoBehaviour {
 
 	public GameObject ObjectToDelete;
 
+	[Tooltip("0 = left click, 1 = right click, 2 = middle click, 3+ is next/back buttons, mmo buttons, etc.")]
 	public int Button = 1;
 
 
---
+++ Assets/Scripts/World/CameraMovementScript.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraMovementScript : MonoBehaviour {
+
+	public static CameraMovementScript MainInstance = null;
+
+	// public static Camera MainCamera = null;
+	private Camera MainCamera = null;
+
+	public float CameraMovementSpeed = 1.0f;
+	public float CameraTurningSpeed = 1.0f;
+
+	[Tooltip("If next camera spot is first spot when at last spot in the list, or if it stops at last spot")]
+	public bool LoopSpots = true;
+	public int CurrentSpot = 0;
+
+	public List<GameObject> CameraSpots;
+
+	void Start() {
+		MainCamera = GetComponent<Camera>();
+		MainInstance = this;
+
+	}
+
+	void Update() {
+
+		transform.position = Vector3.Lerp(
+			transform.position,
+			CameraSpots[CurrentSpot].transform.position,
+			CameraMovementSpeed * Time.deltaTime
+		);
+
+		transform.rotation = Quaternion.Slerp(
+			transform.rotation,
+			CameraSpots[CurrentSpot].transform.rotation,
+			CameraTurningSpeed * Time.deltaTime
+		);
+
+
+	}
+
+	public void NextCameraInternal() {
+		CurrentSpot++;
+
+		if (CurrentSpot >= CameraSpots.Count) {
+			if (LoopSpots) {
+				CurrentSpot = 0;
+			} else {
+				CurrentSpot = CameraSpots.Count - 1;
+			}
+		}
+	}
+
+	public static void NextCamera() {
+		MainInstance.NextCameraInternal();
+	}
+
+	public void PrevCameraInternal() {
+		CurrentSpot--;
+
+		if (CurrentSpot < 0) {
+			if (LoopSpots) {
+				CurrentSpot = CameraSpots.Count - 1;
+			} else {
+				CurrentSpot = 0;
+			}
+		}
+
+	}
+
+	public static void PrevCamera() {
+		MainInstance.PrevCameraInternal();
+	}
+
+}
