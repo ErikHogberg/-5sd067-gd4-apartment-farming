b6be54dd10eff62cbcfd4b44f0771ab4d613eec3
Erik Högberg <erik.hogberg.9945@student.uu.se>
2020-02-17 16:46:59 +0100

    attempt at better mirror

---
+++ Assets/Scripts/InvertedFollowRotationScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvertedFollowRotationScript : MonoBehaviour {
+
+	public GameObject ObjectToFollow;
+
+	private Quaternion initialDirection;
+
+	void Start() {
+		initialDirection = transform.rotation;
+	}
+
+	void Update() {
+
+		transform.rotation =
+			Quaternion.Inverse( ObjectToFollow.transform.rotation);
+			// Quaternion.Euler(Vector3.Reflect(
+			// 	ObjectToFollow.transform.rotation.eulerAngles,
+			// 	initialDirection.eulerAngles
+			// ));
+	}
+
+}
