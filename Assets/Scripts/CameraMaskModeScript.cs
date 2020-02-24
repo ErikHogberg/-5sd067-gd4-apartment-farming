b7abac1484b2cafc4591a6289057fcc24e72040c
Erik Högberg <erik.hogberg.9945@student.uu.se>
2020-02-24 05:04:06 +0100

    pot placing points and menus

---
+++ Assets/Scripts/CameraMaskModeScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMaskModeScript : MonoBehaviour {
+
+	public enum MaskMode {
+		PotSpots,
+		Plants
+	}
+
+	public static CameraMaskModeScript MainInstance;
+
+	private Camera mainCamera;
+
+	public LayerMask PotSpotMask;
+	public LayerMask PlantMask;
+
+	private MaskMode currentMaskMode;
+	public MaskMode CurrentMaskMode {
+		get {
+			return currentMaskMode;
+		}
+
+		set {
+			switch (value) {
+				case MaskMode.PotSpots:
+					mainCamera.cullingMask = PotSpotMask;
+					break;
+				case MaskMode.Plants:
+					mainCamera.cullingMask = PlantMask;
+					break;
+				default:
+					break;
+			}
+
+			currentMaskMode = value;
+		}
+	}
+
+	void Start() {
+		MainInstance = this;
+		mainCamera = GetComponent<Camera>();
+	}
+
+	void Update() {
+
+	}
+}
--- Assets/Scripts/UI/PlantMenuScript.cs
+++ Assets/Scripts/UI/PlantMenuScript.cs
@@ -10,6 +10,8 @@ public class PlantMenuScript : MonoBehaviour {
 
 	void Start() {
 		MainInstance = this;
+		gameObject.SetActive(false);
+
 	}
 
 	void Update() {
@@ -22,9 +24,13 @@ public class PlantMenuScript : MonoBehaviour {
 		currentPot = pot;
 	}
 
-	public void ClearPot(){
+	public void ClearPot() {
 		// TODO: hide menu
 		currentPot = null;
+	}
+
+	public void CloseMenu() {
+		gameObject.SetActive(false);
 	}
 
 }
