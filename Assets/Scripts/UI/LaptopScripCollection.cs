87e666ee04a148eba66ec2e572c9da4eca426478
Erik Norstedt <eriknorth.swe@gmail.com>
2020-02-27 13:45:02 +0100

    Audiomanager and more on the laptop

---
+++ Assets/Scripts/UI/LaptopScripCollection.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LaptopScripCollection : MonoBehaviour {
+
+	[Serializable]
+	public struct DropdownData {
+		public string entry;
+		public GameObject menu;
+
+	}
+
+	public TMP_Dropdown MenuDropdown;
+	public int DefaultEntry = 0;
+
+	public List<DropdownData> entries;
+
+
+	void Start() {
+		MenuDropdown.value = DefaultEntry;
+		DropdownValueChange(DefaultEntry);
+		List<TMP_Dropdown.OptionData> options = new List<TMP_Dropdown.OptionData>();
+		foreach (DropdownData entry in entries) {
+			options.Add(new TMP_Dropdown.OptionData(entry.entry));
+		}
+		MenuDropdown.options = options;
+	}
+
+	void Update() {
+
+	}
+
+	public void DropdownValueChange (int value){
+		HideAll();
+		if (entries[value].menu == null) {
+			return;
+		}
+		entries[value].menu.gameObject.SetActive(true);
+	}
+
+	public void Reset() {
+		MenuDropdown.value = DefaultEntry;
+		//HideAll();
+		DropdownValueChange(MenuDropdown.value);
+	}
+
+	private void HideAll() {
+		foreach (DropdownData entry in entries) {
+			if (entry.menu == null) {
+				continue;
+			}
+			entry.menu.gameObject.SetActive(false);
+		}
+	}
+
+
+
+}
