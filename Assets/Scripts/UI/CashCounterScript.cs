006059e42d5ef59281d065cc93133e12850b05e9
Erik Högberg <erik.hogberg.9945@student.uu.se>
2020-03-05 21:07:19 +0100

    Cash counter in top bar

---
+++ Assets/Scripts/UI/CashCounterScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CashCounterScript : MonoBehaviour
+{
+
+	public static CashCounterScript MainInstance;
+
+	private Text text;
+
+	public string Prefix = "Cash: ";
+	public string Suffix = "kr";
+
+	private void Start() {
+		MainInstance = this;
+		text = GetComponent<Text>();
+	}
+
+	public void SetValue(float value){
+		text.text = Prefix + value + Suffix;
+	}
+
+	public static void SetValueStatic(float value){
+		MainInstance.SetValue(value);
+	}
+
+}
